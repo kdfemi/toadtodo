c167d7bfa35b0639e21980d2ad8984b9e09ce39f
kdfemi <39603632+kdfemi@users.noreply.github.com>
2019-12-02 07:00:02 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDYJSazNQQvWOhcs017qr9Fl69o-oJBYIc',
+    apiKey: '',
     authDomain: 'toadtodo-92e87.firebaseapp.com',
     databaseURL: 'https://toadtodo-92e87.firebaseio.com',
     projectId: 'toadtodo-92e87',
